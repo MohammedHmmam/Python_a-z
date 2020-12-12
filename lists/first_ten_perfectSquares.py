444de0c7560ac17123e5e15237d0d2ed260a22f5
Mohammed Hmmam <mohammedhmmam@gmail.com>
2020-12-13 01:22:36 +0200

    Create a list Containing the first ten perfect squares

---
+++ lists/first_ten_perfectSquares.py
@@ -0,0 +1,11 @@
+#Create a list Containing the first ten perfect squares.
+
+#1- Instantiate an empty list
+#2- loop for a range of elements
+#3- Append each elements to the end of the list
+
+squares = []
+for i in range(10):
+    squares.append(i * i)
+
+print(squares)    
