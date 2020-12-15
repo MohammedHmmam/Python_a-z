6308c656cca0edfd0e8d3fe3230f468b096f5311
Mohammed Hmmam <mohammedhmmam@gmail.com>
2020-12-15 04:40:59 +0200

    Iterating through a string

---
+++ lists/list_comprehension/iterating_throw_string.py
@@ -0,0 +1,11 @@
+#Iterating through a string using for loop
+h_letters = []
+for letter in 'human':
+    h_letters.append(letter)
+print(h_letters)  
+
+print("||||||||||||||||||||||||||||||")
+
+#iterating through a string Using list comprehension
+hu_letters = [letter for letter in 'human']
+print(hu_letters)
